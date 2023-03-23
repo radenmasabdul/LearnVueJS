3650c2daf6bb242b8e1d2ac364e56379cd4fd113
radenmasabdul <abdul.tamsis@gmail.com>
2023-03-24 00:42:33 +0700

    data & method

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
 <template>
   <h1 class="text-red-700 text-4xl text-center">Hello Vue JS</h1>
+
+  <!-- data -->
+  <h2 class="text-center text-xl">Nama : {{ nama }}</h2>
+
+  <!-- method -->
+  <div class="flex justify-center">
+    <button @click="changeName" class="btn btn-info btn-sm text-white rounded">
+      Change Name
+    </button>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      nama: "Abdul Rahman Alhafizh",
+    };
+  },
+  methods: {
+    changeName() {
+      this.nama = "Raden Mas Abdul";
+    },
+  },
+};
+</script>
