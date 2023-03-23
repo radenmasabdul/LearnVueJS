73bccde0f36edb45abbd6a4119ce26c78a724942
radenmasabdul <abdul.tamsis@gmail.com>
2023-03-24 00:50:13 +0700

    data binding

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,25 @@
   <h1 class="text-red-700 text-4xl text-center">Hello Vue JS</h1>
 
   <!-- data -->
-  <h2 class="text-center text-xl">Nama : {{ nama }}</h2>
+  <!-- <h2 class="text-center text-xl">Nama : {{ nama }}</h2> -->
 
   <!-- method -->
-  <div class="flex justify-center">
+  <!-- <div class="flex justify-center">
     <button @click="changeName" class="btn btn-info btn-sm text-white rounded">
       Change Name
+    </button>
+  </div> -->
+
+  <!-- data binding -->
+  <div class="flex justify-center">
+    <button class="btn btn-info btn-sm text-white rounded">
+      <a :href="url">Go To My Website</a>
     </button>
   </div>
 </template>
 
-<script>
+<!-- data & method -->
+<!-- <script>
 export default {
   name: "App",
   data() {
@@ -24,6 +32,18 @@ export default {
     changeName() {
       this.nama = "Raden Mas Abdul";
     },
+  },
+};
+</script> -->
+
+<!-- data binding -->
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      url: "https://radenmasabdul.my.id",
+    };
   },
 };
 </script>
